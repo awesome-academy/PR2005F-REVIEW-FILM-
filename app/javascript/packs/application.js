f2be2450649e5b1ab143f516650bbdf0287074e6
concunmun98 <mstaion508@gmail.com>
2020-10-06 14:00:50 +0700

    Template_Web

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+import "@fortawesome/fontawesome-free/js/all";
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery.nice-select.min
+//= require jquery.slicknav
+//= require main
+//= require mixinup.min
+//= require owl.carousel.min
+//= require player.js
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
