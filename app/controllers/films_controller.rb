6e21c3d02a57c665509bde1c4dc0e4850ad5c71d
concunmun98 <mstaion508@gmail.com>
2020-10-28 10:24:59 +0700

    Film_Post

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,9 +1,9 @@
 class FilmsController < ApplicationController
-
+  before_action :correct_user, only: :destroy
   def index
     
   end
-
+ 
   def show
   	@film = Film.find_by id: params[:id]
     @comment = current_user.comments.build
@@ -13,11 +13,14 @@ class FilmsController < ApplicationController
   	   redirect_to films_index_path
   	end
   end
-
   private
 
   def film_params
-	  params.require(:film).permit(:name, :video)
+	  params.require(:film).permit(:name, :video, :content, :image)
   end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @film.nil?
   
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 	
   def index
+  	@films = Film.all
   end
 
   def contact
