aabe230e233afdd51ad6bd55bdc7a3836bc09af9
concunmun98 <mstaion508@gmail.com>
2020-10-28 19:21:11 +0700

    Update_Reviews_Index

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,7 +1,7 @@
 class FilmsController < ApplicationController
 
   def index
-    
+    @films = Film.all
   end
  
   def show
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
 	belongs_to :film
 	belongs_to :user
 	has_many :comments, as: :cmt_duty
+	has_one_attached :image
 	validates :user_id, presence: true
     validates :content, presence: true
     validates :title, presence: true
